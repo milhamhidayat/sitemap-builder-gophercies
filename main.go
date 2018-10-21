756459e81945cfbbf55d53a2a6c56a79b5ce0717
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-10-21 21:01:47 +0700

    get webpage

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-	urlFlag := flag.String("url", "https://gophercies.com", "the url that you want to build a sitemap for")
+	urlFlag := flag.String("url", "https://gophercises.com/", "the url that you want to build a sitemap for")
 	flag.Parse()
 	fmt.Println(*urlFlag)
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
