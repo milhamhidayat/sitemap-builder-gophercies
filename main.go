0755d2d46ad2a493ed1a461accfa5a0ea8b3fa5f
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-11-08 20:45:14 +0700

    Parsing and building links

--- main.go
+++ main.go
@@ -3,20 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
+	"html-link-parser-gophercies/link"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 )
 
 func main() {
-	urlFlag := flag.String("url", "https://gophercises.com/", "the url that you want to build a sitemap for")
+	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
 	flag.Parse()
-	fmt.Println(*urlFlag)
 
 	resp, err := http.Get(*urlFlag)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqUrl := resp.Request.URL
+	links, _ := link.Parse(resp.Body)
+
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+
+	base := baseUrl.String()
+
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		// handle /something
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		// handle https/http
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+
 }
